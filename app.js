d795186988ac9b9a06c3720562dc2d23a9f1ad1a
ParvizNajimov <101324794+ParvizNajimov@users.noreply.github.com>
2022-11-22 21:11:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let products = [
+    {
+        name: 'milk',
+        type: 'milk',
+        price: 10000
+    },
+    {
+        name: 'apple',
+        type: 'fruit',
+        price: 6000
+    },
+    {
+        name: 'vodka',
+        type: 'alcohol',
+        price: 70000
+    },
+    {
+        name: 'tvorog',
+        type: 'milk',
+        price: 15000
+    },
+    {
+        name: 'banan',
+        type: 'fruit',
+        price: 25000
+    },
+    {
+        name: 'pivo',
+        type: 'alcohol',
+        price: 50000
+    },
+    {
+        name: 'cheese',
+        type: 'milk',
+        price: 40000
+    },
+    {
+        name: 'persik',
+        type: 'fruit',
+        price: 23000
+    },
+    {
+        name: 'mochito',
+        type: 'alcohol',
+        price: 120000
+    },
+]
+
+// let maxP = []
+
+
+
+// for(let maxPrice of prod1ucts) {
+//     let key = maxPrice.price
+//     if ( key.Math.max() > key.Math.max() ) {
+//         key = maxP
+//     }
+// }
+
+// console.log(maxP);
+
+
+
+// for(let item of  products) {
+//     let key = item.Math.max(price) 
+//     if(item.price > key) {
+//      item.price.push(maxP)
+//     }
+
+
+// }
+// console.log(maxP);
